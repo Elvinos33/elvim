0b5b05b95f939141c0672fb8a35ce3f19ddbd8a6
elvinos <elvinos33@duck.com>
2023-10-14 19:00:14 +0200

    Added Harpoon & lualine with devicons

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,21 +13,26 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-  'williamboman/mason.nvim',
-  'williamboman/mason-lspconfig.nvim',
-  'neovim/nvim-lspconfig',
-  'hrsh7th/nvim-cmp',
-  'hrsh7th/cmp-nvim-lsp',
-  'L3MON4D3/LuaSnip',
-  'saadparwaiz1/cmp_luasnip',
-  'rafamadriz/friendly-snippets',
-  'nvim-treesitter/nvim-treesitter',
-  'ellisonleao/gruvbox.nvim',
-  {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.0',
-    dependencies = { {'nvim-lua/plenary.nvim'} },
-  },
+   'williamboman/mason.nvim',
+   'williamboman/mason-lspconfig.nvim',
+   'neovim/nvim-lspconfig',
+   'hrsh7th/nvim-cmp',
+   'hrsh7th/cmp-nvim-lsp',
+   'L3MON4D3/LuaSnip',
+   'saadparwaiz1/cmp_luasnip',
+   'rafamadriz/friendly-snippets',
+   'nvim-treesitter/nvim-treesitter',
+   'ThePrimeagen/harpoon',
+   'ellisonleao/gruvbox.nvim',
+   {
+      'nvim-lualine/lualine.nvim',
+      dependencies = { 'nvim-tree/nvim-web-devicons', opt = true }
+   },
+   {
+      'nvim-telescope/telescope.nvim',
+      tag = '0.1.0',
+      dependencies = { {'nvim-lua/plenary.nvim'} },
+   },
 }
 
 local opts = {}
