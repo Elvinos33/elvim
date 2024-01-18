e8be9bb45e2007ecafd46b1ecafed3b0f2cff465
Elvinos33 <elvinos33@duck.com>
2024-01-18 20:39:04 +0100

    Actually added config for treesitter and changes to neoformat

--- lua/config/plugin_configs/neoformat.lua
+++ lua/config/plugin_configs/neoformat.lua
@@ -1,3 +1,5 @@
 vim.api.nvim_set_keymap('n', '<leader>ft', ':Neoformat prettier<Enter>', { noremap = true, silent = true, desc = '[F]ormat [S]ode' })
 
+vim.g.neoformat_try_node_exe = 1
+
 vim.cmd('autocmd BufWritePre *.js,*.jsx,*.ts,*.tsx,*.svelte,*.astro,*.vue :Neoformat prettier')
