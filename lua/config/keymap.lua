2df10e6d69edf7b5312110a7ff9859113a29df2b
elvin-baloku <elvin.baloku@schibsted.com>
2024-08-28 11:05:03 +0200

    Changed desc on command in keymap

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -14,8 +14,7 @@ vim.keymap.set('n', '<leader>fb', fzfLua.buffers, {})
 --Neogit Bindings
 ------------------------------------------------------------------
 vim.keymap.set('n', '<leader>g', ":Neogit<CR>", { desc = 'Open Neogit'})
-vim.keymap.set('n', '<leader>g', ":Neogit<CR>:call feedkeys('Scc')<CR>", { desc = 'Open Neogit'})
-vim.keymap.set('n', '<leader>gc', ":Neogit commit<CR>", { desc = 'Open Neogit-commit' })
+vim.keymap.set('n', '<leader>gc', ":Neogit<CR>:call feedkeys('Scc')<CR>", { desc = 'Open Neogit and Stage all commit and open commit popup'})
 ------------------------------------------------------------------
 
 --Oil Bindings
