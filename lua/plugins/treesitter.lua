5a4cd72a37552ae00e6cf71ec3670d7bab73d989
elvin-baloku <elvin.baloku@schibsted.com>
2024-10-09 13:41:51 +0200

    Fixed Treesitter again

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,25 +5,28 @@ return {
   version = false,
   build = ":TSUpdate",
   event = { "BufReadPost", "BufNewFile" },
-  opts = {
-    sync_install = false,
-    auto_install = true,
-    ensure_installed = {
-      "typescript",
-      "go",
-      "rust",
-      "lua",
-      "vim",
-      "vimdoc",
-      "astro",
-      "tsx",
-      "terraform",
-      "svelte",
-    },
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-    },
-    indent = { enable = true },
-  }
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      sync_install = false,
+      auto_install = true,
+      ensure_installed = {
+        "typescript",
+        "go",
+        "rust",
+        "lua",
+        "vim",
+        "vimdoc",
+        "astro",
+        "tsx",
+        "terraform",
+        "svelte",
+      },
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      indent = { enable = true },
+
+    })
+  end
 }
