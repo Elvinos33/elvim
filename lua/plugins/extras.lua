723f7301358c397bdf5a97b3b264ffa4315307c0
Anders Haarr <anders.haarr@vg.no>
2025-04-24 00:14:03 +0200

    remove dropbar

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -13,20 +13,20 @@ return {
     "echasnovski/mini.diff",
     opts = {},
   },
-  {
-    "Bekaboo/dropbar.nvim",
-    -- optional, but required for fuzzy finder support
-    dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-    },
-    config = function()
-      local dropbar_api = require("dropbar.api")
-      vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
-      vim.keymap.set("n", "<leader>æ", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
-      vim.keymap.set("n", "<leader>ø", dropbar_api.select_next_context, { desc = "Select next context" })
-    end,
-  },
+  -- {
+  --   "Bekaboo/dropbar.nvim",
+  --   -- optional, but required for fuzzy finder support
+  --   dependencies = {
+  --     "nvim-telescope/telescope-fzf-native.nvim",
+  --     build = "make",
+  --   },
+  --   config = function()
+  --     local dropbar_api = require("dropbar.api")
+  --     vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
+  --     vim.keymap.set("n", "<leader>æ", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
+  --     vim.keymap.set("n", "<leader>ø", dropbar_api.select_next_context, { desc = "Select next context" })
+  --   end,
+  -- },
   {
     "folke/noice.nvim",
     event = "VeryLazy",
