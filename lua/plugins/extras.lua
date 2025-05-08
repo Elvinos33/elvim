4f9a4bb227e6ef25b291d9c4bc6ecf32f85379bb
Anders Haarr <anders.haarr@vg.no>
2025-05-08 10:15:10 +0200

    add tmux-config

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -5,6 +5,24 @@ return {
     opts = {},
   },
   {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
+  {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
     opts = { disable_filetype = { "codecompanion", "snacks_input" } },
