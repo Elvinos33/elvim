8d63711531ed03077d329239a9d83838346aa49e
Anders Haarr <anders.haarr@vg.no>
2025-05-28 09:32:29 +0200

    fix: breaking change mason-lspconfig

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -3,9 +3,9 @@ return {
   "neovim/nvim-lspconfig", -- LSP
   event = { "BufReadPre", "BufNewFile" },
   dependencies = {
-    { "williamboman/mason.nvim",          config = true }, -- For installing and managing LSPs
-    { "williamboman/mason-lspconfig.nvim" },               -- To use Mason with native LSP
-    { "hrsh7th/cmp-nvim-lsp" },                            -- For LSP completion
+    { "williamboman/mason.nvim", config = true }, -- For installing and managing LSPs
+    { "williamboman/mason-lspconfig.nvim" }, -- To use Mason with native LSP
+    { "hrsh7th/cmp-nvim-lsp" }, -- For LSP completion
   },
   config = function()
     -- Setup Mason
@@ -20,29 +20,31 @@ return {
           package_installed = "✓",
           package_pending = "⟳",
           package_uninstalled = "✗",
-        }
+        },
       },
     })
 
     require("mason-lspconfig").setup({
-      ensure_installed = vim.tbl_keys(require("plugins.lsp-servers.servers"))
+      ensure_installed = vim.tbl_keys(require("plugins.lsp-servers.servers")),
     })
 
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-    require("mason-lspconfig").setup_handlers({
-      function(server)
-        local server_opts = require("plugins.lsp-servers.servers")[server] or {}
-        server_opts.capabilities = capabilities
-
-        require("lspconfig")[server].setup(server_opts)
-      end
+    require("mason-lspconfig").setup({
+      ensure_installed = vim.tbl_keys(require("plugins.lsp-servers.servers")),
+      handlers = {
+        function(server_name)
+          local server_opts = require("plugins.lsp-servers.servers")[server_name] or {}
+          server_opts.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server_opts.capabilities or {})
+          require("lspconfig")[server_name].setup(server_opts)
+        end,
+      },
     })
 
     for server, config in pairs(require("plugins.lsp-servers.custom")) do
       require("lspconfig.configs")[server] = config
       require("lspconfig")[server].setup(config)
     end
-  end
+  end,
 }
