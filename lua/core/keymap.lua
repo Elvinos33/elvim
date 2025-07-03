b8ebab9bc5577d50c0f50119924723afae9dede8
Anders Haarr <anders.haarr@vg.no>
2025-07-03 21:00:37 +0200

    useful maps

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -27,6 +27,24 @@ vim.keymap.set("n", "gE", vim.diagnostic.open_float, { desc = "Show diagnostic m
 vim.keymap.set(
   "n",
   "<leader>vf",
-  [[:%!jq -r 'to_entries | .[] | "\(.key)=\(.value)"'<CR>]],
+  [[:%!jq -r 'to_entries | .[] | "\(.key)=\(.value|tojson)"'<CR>]],
   { desc = "Convert a vault json object to .env" }
+)
+-- Copy file structure of wdir in normal mode
+vim.keymap.set(
+  "n",
+  "L",
+  function()
+    -- Exclude common directories such as node_modules, .git, .venv, etc.
+    local exclude = { "node_modules", ".git", ".venv", ".cache", "dist", "build" }
+    local find_cmd = { "find", ".", "-type", "f" }
+    for _, dir in ipairs(exclude) do
+      table.insert(find_cmd, "!")
+      table.insert(find_cmd, "-path")
+      table.insert(find_cmd, "./" .. dir .. "/*")
+    end
+    local result = vim.fn.system(find_cmd)
+    vim.fn.setreg("+", result)
+  end,
+  { noremap = true, silent = true, desc = "Copy file paths recursively to system clipboard, excluding common paths" }
 )
