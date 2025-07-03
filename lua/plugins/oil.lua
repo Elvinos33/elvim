9388bd9f762fbc8d6d227cac92d702894fa71380
Anders Haarr <anders.haarr@vg.no>
2025-07-03 21:27:43 +0200

    oil-git status

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,22 +1,29 @@
 return {
-  "stevearc/oil.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {
-    default_file_explorer = true,
-    delete_to_trash = true,
-    skip_confirm_for_simple_edits = true,
-    columns = { "icon" },
-    view_options = {
-      show_hidden = true,
-      natural_order = true,
-      is_always_hidden = function(name, _)
-        return name == ".." or name == ".git"
-      end,
+  {
+    "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      default_file_explorer = true,
+      delete_to_trash = true,
+      skip_confirm_for_simple_edits = true,
+      columns = { "icon" },
+      view_options = {
+        show_hidden = true,
+        natural_order = true,
+        is_always_hidden = function(name, _)
+          return name == ".." or name == ".git"
+        end,
+      },
     },
+    config = function(_, opts)
+      local oil = require("oil")
+      oil.setup(opts)
+      vim.keymap.set("n", "<leader>o", oil.open, { desc = "Toggle Oil" })
+    end,
   },
-  config = function(_, opts)
-    local oil = require("oil")
-    oil.setup(opts)
-    vim.keymap.set("n", "<leader>o", oil.open, { desc = "Toggle Oil" })
-  end,
+  {
+    "benomahony/oil-git.nvim",
+    dependencies = { "stevearc/oil.nvim" },
+    -- No opts or config needed! Works automatically
+  },
 }
