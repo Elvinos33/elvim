9869e77069916289dec98ba1cdc3edd422df18c7
Anders Haarr <anders.haarr@vg.no>
2025-07-26 22:06:42 +0200

    add image in snacks, add plugin which-key

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,6 +6,7 @@ return {
   opts = {
     bigfile = { enabled = true },
     dashboard = { enabled = true },
+    image = { enabled = true },
     explorer = { enabled = true },
     indent = { enabled = true },
     input = { enabled = true },
@@ -21,7 +22,7 @@ return {
     words = { enabled = true },
     styles = {
       notification = {
-        -- wo = { wrap = true } -- Wrap notifications
+        wo = { wrap = true }, -- Wrap notifications
       },
     },
   },
