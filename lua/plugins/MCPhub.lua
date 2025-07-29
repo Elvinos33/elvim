344880773022ca24b3b9d4fdfa413b0b3fbbe8f8
Anders Haarr <anders.haarr@vg.no>
2025-07-29 22:39:30 +0200

    hotkey for mcp

--- lua/plugins/MCPhub.lua
+++ lua/plugins/MCPhub.lua
@@ -3,6 +3,9 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
+  keys = {
+    { "<leader>mc", "<cmd>MCPHub<cr>", desc = "Open MCPHub" },
+  },
   build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
   config = function()
     require("mcphub").setup()
